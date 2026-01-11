5fc806ca482c82b5c763a89d318f63813d60c1d6
Khyati Sharma <khyatishar10949@gmail.com>
2026-01-11 22:51:29 +0530

    Improve pandas CSV reading with advanced parameters

--- ML_d2/csv_datahandling.py
+++ ML_d2/csv_datahandling.py
@@ -31,8 +31,8 @@ print("\nShape:",df.shape)
 print("\nStatistical summary:",df.describe())
 print("\nMissing values:",df.isnull().sum())
 
-# Loading a TSV (tab-separated values) file with custom column names(since we didnt hsve column names in the dataset);(default sep=', so we need to specify sep='\t')
 # sep and names parameters
+# Loading a TSV (tab-separated values) file with custom column names(since we didnt hsve column names in the dataset);(default sep=', so we need to specify sep='\t')
 df_movies = pd.read_csv('movie_titles_metadata.tsv', sep='\t', names=['sno','name','release_year','rating','votes','genres'])
 print("\nMovies Dataset info:")
 df_movies.info()
@@ -40,21 +40,22 @@ print("\nShape:",df_movies.shape)
 print("\nStatistical summary:",df_movies.describe())
 print("\nMissing values:",df_movies.isnull().sum())
 
-# Loading a CSV file with an index column(Usually done when the dataset has a unique identifier column that can be used for indexing and you dont want to use the default integer index)
 # index_col parameter
+# Loading a CSV file with an index column(Usually done when the dataset has a unique identifier column that can be used for indexing and you dont want to use the default integer index)
 df_train = pd.read_csv('aug_train.csv',index_col='enrollee_id')
 
-# if the first row contains column names but pandas fails to recognize it due to formatting issues, you can use the header parameter to specify the correct row 
 # header parameter
+# if the first row contains column names but pandas fails to recognize it due to formatting issues, you can use the header parameter to specify the correct row 
 df_test = pd.read_csv("test.csv", header=1)
 df_test.info()
 
+# header parameter
 #if you only want certain columns from the dataset, you can use the usecols parameter to load only those specific columns
-# usecols parameter
 df_cols = pd.read_csv('aug_train.csv',usecols=['enrollee_id','gender','education_level'])
 print(df_cols.head())
 
-# na_values parameter=If the dataset uses a specific string to denote missing values (e.g., "NA", "N/A", "missing"), you can use the na_values parameter to specify these strings so that pandas can correctly identify and handle them as NaN.this is useful if you want to apply NaN handling techniques later.
+# na_values parameter
+# If the dataset uses a specific string to denote missing values (e.g., "NA", "N/A", "missing"), you can use the na_values parameter to specify these strings so that pandas can correctly identify and handle them as NaN.this is useful if you want to apply NaN handling techniques later.
 df_na = pd.read_csv('aug_train.csv', na_values=['NA', 'N/A', 'missing'])
 print("\nMissing values after na_values parameter:")
 print(df_na.isnull().sum())
